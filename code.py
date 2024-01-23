01f69266b550268e36e9a2f11741a8494b0b39d6
LucasSanto11 <156825828+LucasSanto11@users.noreply.github.com>
2024-01-23 16:25:16 +0100

    Update code.py
    Ajout de la fonction rejeu et défaite lorsque points > 21

--- code.py
+++ code.py
@@ -1,28 +1,30 @@
-print("Bienvenue au Blackjack")#message de bienvenue sur le jeux + animation après#
-sous = [0]
+print("Bienvenue au Blackjack") #message de bienvenue sur le jeux + animation après
+sous = 0
+valeur=0
+points=0
 def miser(sous):
     if sous > 0 and sous <= 300:
         return sous
     else:
         print("Vous ne pouvez pas miser plus de 300 €")
 sous = int(input("Combien d'euros souhaitez-vous miser ? "))
-miser(sous)#16/01/2024
-#fonction permettant de donner une mise #(voir à la fin de la partie gains/pertes)
+miser(sous) #16/01/2024 fonction permettant de donner une mise (voir à la fin de la partie gains/pertes)
 
 
 import random
 
+As=11 #valeur temporaire à changer
+
 cartes=['As', 'Deux', 'Trois', 'Quatre', 'Cinq', 'Six', 'Sept', 'Huit', 'Neuf', 'Dix', 'Valet', 'Dame', 'Roi']
 symbole=[' de Pique', ' de Carreaux', ' de Coeur', " de Trèfle"]
 random.randint(0, 12)
-
 
 def tirage():
     val=[As, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     rand1=random.randint(0, 12)
     rand2=random.randint(0, 3)
     print("Vous avez", points, "points.")
-    if rand1==0 and 11+points<21:
+    if rand1==0 and 11+points<21: #si on tire un as et que nb points + 11 est inférieur à 21
         valeur=int(input("Choisir une valeur entre 1 et 11"))
         while valeur!=1:
             valeur=0
@@ -33,14 +35,27 @@ def tirage():
     print(carte)
     return valeur
 
-valeur=0
-points=0
-for i in range(20):
+
+def rejeu():
+    global points
+    print("Tirer ou rester ?") #à la fin de la fonction croupier demander au joueur de jouer
+    rep=input("Tirer ou rester ?")
+    if rep.lower()=="tirer":
+        valeur=(tirage())
+        points+=valeur
+        if points<21:
+            print("Vous avez", points, "points")
+            rejeu()
+        elif points>21:
+            print("Vous avez", points, "points, vous avez dépassé 21 points")
+            print("Voulez vous relancer une partie ?")
+
+    else:
+        print("à faire")
+
+
+for i in range(1):
     valeur=(tirage())
     points+=valeur
 print("Vous avez", points, "points")
-
-
-
-print("à vous de jouer")
-#à la fin de la fonction croupier demander au joueur de jouer
+rejeu()
