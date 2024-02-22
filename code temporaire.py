e6f173114c7b5c052efea306053e01b7b164a370
LucasSanto11 <156825828+LucasSanto11@users.noreply.github.com>
2024-02-22 02:10:29 +0100

    Update code temporaire.py

--- code temporaire.py
+++ code temporaire.py
@@ -24,7 +24,7 @@ cartes=['As', 'Deux', 'Trois', 'Quatre', 'Cinq', 'Six', 'Sept', 'Huit', 'Neuf', 
 symbole=[' de Pique', ' de Carreaux', ' de Coeur', " de Trèfle"]
 random.randint(0, 12)
 
-def demande_retirage():
+def demande_rejeu():
     rep=input("Voulez vous rejouer et tenter d'obtenir un gain plus conséquent :) ? Répondez par 'OUI' ou 'NON' ")
     if rep.upper()=='OUI' :
             print('On relance une partie alors!')
@@ -43,7 +43,7 @@ def tirage_croupier(): #le croupier tire
         print('')
     elif points_croupier==21:
         print('le croupier a obtenu 21 points, vous avez perdu votre mise ;(')
-        demande_retirage()
+        demande_rejeu()
 
     else: #Si le croupier a moins de 17 points, il tire une carte
         if rand1==0 : #si on tire un as 
@@ -120,23 +120,23 @@ def retirage():
         valeur=(tirage())
         points+=valeur
         if points<21:
-            retirage()
-        elif points>21:
+            retirage() #le joueur a la possibilité de rejouer, on re demande s'il veut Tirer ou Rester
+        elif points>21: 
             print("Vous avez", points, "points, vous avez perdu ;( !")
-            demande_retirage()
+            demande_rejeu() #Le joueur a dépassé 21 points, a perdu, on lui propose de relancer une partie
         elif points==21:
             sous=2*sous
             print('Vous avez obtenu le score parfait! Vous avez maintenant une mise de',sous,';)')
-            demande_retirage()
+            demande_rejeu() #Le joueur a gagné, on lui propose de relancer une partie
 
     else: #si le joueur décide de rester
         if points<points_croupier: #Si le joueur a moins de points que le croupier
              print ('Oh mince, vous avez perdu votre mise, vous n*avez plus rien !')
-             demande_retirage()
+             demande_rejeu()
         else : #Si le joueur a plus de points que le croupier
              sous=2*sous
              print('Bravo, vous avez gagné :) ! Votre mise est maintenant de', sous,'!')
-             demande_retirage()
+             demande_rejeu()
 
 
 #Lance la partie et tire deux cartes
